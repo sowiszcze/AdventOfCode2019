6db66735fda4450fd977783d094f77233593b953
Piotr Sowa (PIOA) <pioa@dgs.com>
2019-12-06 07:45:08 +0100

    Finished day 2

---
+++ Day02Task2Result/Program.cs
@@ -0,0 +1,32 @@
+﻿using Day02Task1Solution;
+using System;
+
+namespace Day02Task2Result
+{
+    class Program
+    {
+        private const int ExpectedOutput = 19690720;
+
+        static void Main(string[] args)
+        {
+            int output;
+
+            for (int noun = 0; noun < 100; noun++)
+            {
+                for (int verb = 0; verb < 100; verb++)
+                {
+                    output = Solution.Calculate(Solution.Fix(Data.Program, noun, verb)).GetResult();
+
+                    if (output == ExpectedOutput)
+                    {
+                        Console.WriteLine($"The result is: {100 * noun + verb}");
+
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("No result. Something went wrong!");
+        }
+    }
+}
