5337febb96f3ff4017fb0d06bda032b18d2c9a66
Piotr Sowa (PIOA) <pioa@dgs.com>
2019-12-06 07:28:37 +0100

    Additional cleanup for first task of second day in preparation for second task

--- Day02Task1Result/Program.cs
+++ Day02Task1Result/Program.cs
@@ -7,7 +7,7 @@ namespace Day02Task1Result
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"The result is: {Solution.Calculate(Solution.Fix(Data.Program)).GetResult()}");
+            Console.WriteLine($"The result is: {Solution.Calculate(Solution.Fix(Data.Program, 12, 2)).GetResult()}");
         }
     }
 }
--- Day02Task1Solution/Solution.cs
+++ Day02Task1Solution/Solution.cs
@@ -10,20 +10,20 @@ namespace Day02Task1Solution
             int instruction,
                 noun,
                 verb,
-                resultIndex;
+                outputIndex;
             for (int index = 0; memory[index] != 99; index += 4)
             {
                 instruction = memory[index];
                 noun = memory[memory[index + 1]];
                 verb = memory[memory[index + 2]];
-                resultIndex = memory[index + 3];
+                outputIndex = memory[index + 3];
                 switch (instruction)
                 {
                     case 1: // addition
-                        memory[resultIndex] = noun + verb;
+                        memory[outputIndex] = noun + verb;
                         break;
                     case 2: // addition
-                        memory[resultIndex] = noun * verb;
+                        memory[outputIndex] = noun * verb;
                         break;
                     default:
                         throw new ArgumentException($"Unsupported instruction {instruction} at index {index}.");
@@ -32,11 +32,11 @@ namespace Day02Task1Solution
             return memory;
         }
 
-        public static int[] Fix(int[] program)
+        public static int[] Fix(int[] program, int noun, int verb)
         {
             int[] memory = program.Clone() as int[];
-            memory[1] = 12;
-            memory[2] = 2;
+            memory[1] = noun;
+            memory[2] = verb;
             return memory;
         }
 
