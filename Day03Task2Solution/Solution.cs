2d1c7e4055f4b639c9e89210203e70f5aaf3b5e9
Piotr Sowa (PIOA) <pioa@dgs.com>
2019-12-06 14:25:32 +0100

    Second task of third day - started progress

---
+++ Day03Task2Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day03Task2Solution
+{
+    public static class Solution
+    {
+        public static IList<(char, int)> CreateCommandsList(string input)
+        {
+            return input.Split(',').Select(e => (e[0], int.Parse(e.Substring(1)))).ToList();
+        }
+
+
+    }
+}
