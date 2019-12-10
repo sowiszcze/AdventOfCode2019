0243d6baf9ce8ccfe66fb048684caed7172da733
Piotr Sowa (PIOA) <pioa@dgs.com>
2019-12-10 13:55:08 +0100

    Day ninth finished

---
+++ Day09Task2Result/Program.cs
@@ -0,0 +1,15 @@
+﻿using Day05Task1Solution;
+using System;
+using System.Linq;
+
+namespace Day09Task2Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Solution.Run(Data.Program, 2L);
+            Console.WriteLine($"The result is: {result.Last()}");
+        }
+    }
+}
