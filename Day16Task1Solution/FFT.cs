19637e072879a9e2bceb6907a0bba02ce10f0f90
Piotr Sowa <mail@sowiszcze.pl>
2019-12-16 23:36:41 +0100

    Second task of sixteenth day in progress

--- Day16Task1Solution/FFT.cs
+++ Day16Task1Solution/FFT.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Day16Task1Solution
 {
@@ -12,6 +13,12 @@ namespace Day16Task1Solution
             this.pattern = pattern;
         }
 
+        public string DecodeWithOffset(string encoded, int phases, int length)
+        {
+            var offset = int.Parse(encoded.Substring(0, 7));
+            return string.Join("", Decode(string.Join("", Enumerable.Repeat(encoded, 10000)), phases).Skip(offset).Take(length));
+        }
+
         public int[] Decode(string encoded, int phases)
         {
             return Decode(encoded.ToCharArray().Select(e => (int)(e - '0')).ToArray(), phases);
@@ -20,17 +27,28 @@ namespace Day16Task1Solution
         public int[] Decode(int[] encoded, int phases)
         {
             var current = encoded.Clone() as int[];
+            var calculatedPhases = new int[encoded.Length][];
+
+            for (var d = 0; d < encoded.Length; d++)
+            {
+                calculatedPhases[d] = new int[encoded.Length];
+                for (var i = 0; i < encoded.Length; i++)
+                {
+                    calculatedPhases[d][i] = pattern[Convert.ToInt32(Math.Floor((i + 1.0) / (d + 1.0))) % pattern.Length];
+                }
+            }
+
             for (int p = 0; p < phases; p++)
             {
                 var calculated = new int[current.Length];
-                for (var d = 0; d < current.Length; d++)
+                Parallel.For(0, current.Length, (d) =>
                 {
                     calculated[d] = Math.Abs(current.Select((e, i) => new
                     {
                         Digit = e,
-                        Index = Convert.ToInt32(Math.Floor((i + 1.0) / (d + 1.0))) % pattern.Length
-                    }).Sum(i => i.Digit * pattern[i.Index])) % 10;
-                }
+                        Multiplier = calculatedPhases[d][i]
+                    }).Sum(i => i.Digit * i.Multiplier)) % 10;
+                });
                 current = calculated;
             }
             return current;
---
+++ Day16Task2Tests/Tests.cs
@@ -0,0 +1,19 @@
+using Day16Task1Solution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day16Task2Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 0, 1, 0, -1 }, "03036732577212944063491565474664", 100, 8, "84462026")]
+        [DataRow(new int[] { 0, 1, 0, -1 }, "02935109699940807407585447034323", 100, 8, "78725270")]
+        [DataRow(new int[] { 0, 1, 0, -1 }, "03081770884921959731165446850517", 100, 8, "53553731")]
+        public void CheckForCorrectResult(int[] pattern, string encoded, int phases, int length, string expectedResult)
+        {
+            var fft = new FFT(pattern);
+            Assert.AreEqual(expectedResult, fft.DecodeWithOffset(encoded, phases, length));
+        }
+    }
+}
